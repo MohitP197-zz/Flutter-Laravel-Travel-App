0989912b002415dbcfeaeeff69d120d1513e2e0b
Mahendra Paudel <paudel823@gmail.com>
2019-08-09 11:26:44 +0545

    Updated main page

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,140 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Color(0xFF18D191)),
+                  child: Icon(
+                    Icons.bookmark,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(right: 50.0, top: 70.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Color(0xFFFC6A7F)),
+                  child: Icon(
+                    Icons.local_airport,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 30.0, top: 70.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Color(0xFFFFCE56)),
+                  child: Icon(
+                    Icons.local_taxi,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 110.0, top: 40.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Color(0xFF45E0EC)),
+                  child: Icon(
+                    Icons.local_offer,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 90.0),
+                  child: Text(
+                    "Travel App",
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 20.0, top: 10.0),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 70.0,
+                      width: 70.0,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF18D191),
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Text(
+                        "Sign In With Email",
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 20.0, right: 5.0, top: 10.0),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 70.0,
+                      width: 70.0,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF4364A1),
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Text(
+                        "Facebooks",
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 10.0, right: 20.0, top: 10.0),
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 70.0,
+                      width: 70.0,
+                      decoration: BoxDecoration(
+                          color: Color(0xFFDF513B),
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: Text(
+                        "Google",
+                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
