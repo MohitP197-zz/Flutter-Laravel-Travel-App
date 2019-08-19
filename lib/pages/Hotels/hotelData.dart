e08352a34f6a629934660ebdb29e1ffa492abae9
Mahendra Paudel <paudel823@gmail.com>
2019-08-19 16:04:33 +0545

    Updates

---
+++ lib/pages/Hotels/hotelData.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+List<String> sizeNumlist = [
+  "7",
+  "8",
+  "9",
+  "10",
+];
+
+List<Color> colors = [
+  Color(0xFFF9362E),
+  Color(0xFF003CFF),
+  Color(0xFFFFB73A),
+  Color(0xFF3AFFFF),
+  Color(0xFF1AD12C),
+  Color(0xFFD66400),
+];
+
+String desc = 
+    "Hey Adidas"
+    "Hey Nike"
+    "Hey New Balance";
--- lib/pages/Hotels/hotelScreenBottomPart.dart
+++ lib/pages/Hotels/hotelScreenBottomPart.dart
@@ -1,10 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:travelapp/pages/Hotels/hotelData.dart';
+import 'package:travelapp/utils/utils.dart';
 
-class HomeScreenBottomPart extends StatelessWidget {
+class HomeScreenBottomPart extends StatefulWidget {
+  @override
+  _HomeScreenBottomPartState createState() => _HomeScreenBottomPartState();
+}
+
+class _HomeScreenBottomPartState extends State<HomeScreenBottomPart> {
+  bool isExpanded = false;
+  void _expand() {
+    setState(() {
+      isExpanded ? isExpanded = false : isExpanded = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: screenAwareSize(18.0, context)),
+            child: Text(
+              "Product Details",
+              style: TextStyle(
+                  color: Color(0xFF949598),
+                  fontSize: screenAwareSize(10.0, context),
+                  fontFamily: "Montserrat-SemiBold"),
+            ),
+          ),
+          SizedBox(
+            height: screenAwareSize(8.0, context),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                left: screenAwareSize(26.0, context),
+                right: screenAwareSize(18.0, context)),
+            child: AnimatedCrossFade(
+              firstChild: Text(
+                desc,
+                maxLines: 2,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenAwareSize(10.0, context),
+                    fontFamily: "Montserrat-Medium"),
+              ),
+              secondChild: Text(
+                desc,
+                maxLines: 2,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: screenAwareSize(10.0, context),
+                    fontFamily: "Montserrat-Medium"),
+              ),
+              crossFadeState: isExpanded
+                  ? CrossFadeState.showSecond
+                  : CrossFadeState.showFirst,
+              duration: kThemeAnimationDuration,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                left: screenAwareSize(26.0, context),
+                right: screenAwareSize(18.0, context)),
+            child: GestureDetector(
+                onTap: _expand,
+                child: Text(
+                  isExpanded ? "less" : "more...",
+                  style: TextStyle(
+                      color: Color(0xFFFB382F), fontWeight: FontWeight.w700),
+                )),
+          )
+        ],
+      ),
     );
   }
 }
